05f7d9d101168793f70788769dc920fb8fb4adfe
mukul-329 <mukulsengar329@gmail.com>
2023-08-11 17:41:55 +0000

    updated resource attributes referencse

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,28 @@
 output "subnets" {
   value = azurerm_subnet.mod1
+  description="Attributes reference to the subnet"
 }
 output "virtual_network" {
   value = azurerm_virtual_network.mod1
+  description="Attributes reference for virtual network"
 }
 output "resource_group" {
   value=azurerm_resource_group.mod1
+  description="Attributes reference for the resource group"
 }
 output "public_ip" {
   value=azurerm_public_ip.mod1
+  description="Attributes reference for the public ip"
 }
 output "network_security_group" {
   value=azurerm_network_security_group.mod1
+  description="Attributes reference for the network security group and security rules"
 }
 output "network_interface" {
   value = azurerm_network_interface.mod1
+  description="Attributes reference for network interface card"
 }
 output "virtual_machine" {
   value=azurerm_virtual_machine.mod1
+  description="Attributes reference for virtual machine"
 }
